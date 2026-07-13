288db3bcd35658137de5729cc544ec913dc7fc2d
mahinfathima05-wq <mahinfathima05@gmail.com>
2026-07-13 21:57:58 +0530

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("loginForm");
-
-form.addEventListener("submit", (e)=>{
-
-    e.preventDefault();
-
-    alert("Login system will be connected to Firebase next.");
-
-});
