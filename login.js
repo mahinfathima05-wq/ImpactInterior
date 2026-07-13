ecb3a5df19af9a2ecf89989fd579e2e710fa0da7
mahinfathima05-wq <mahinfathima05@gmail.com>
2026-07-13 14:03:12 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", (e)=>{
+
+    e.preventDefault();
+
+    alert("Login system will be connected to Firebase next.");
+
+});
